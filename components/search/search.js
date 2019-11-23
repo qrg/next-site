c229110c2ddfb0168a302f277412d5442f403cf8
Luis Alvarez <luisito453@gmail.com>
2019-11-22 19:11:55 -0500

    Avoid multiple calls with the same value to Algolia

--- components/search/search.js
+++ components/search/search.js
@@ -1,10 +1,11 @@
+import { memo } from 'react';
 import { InstantSearch, Configure } from 'react-instantsearch-dom';
 import AlgoliaClient from '../../lib/algolia-client';
 import AutoComplete from './auto-complete';
 
 const searchClient = new AlgoliaClient();
 
-export default function Search(props) {
+function Search(props) {
   return (
     <InstantSearch indexName="nextjs_docs" searchClient={searchClient}>
       <Configure hitsPerPage={8} />
@@ -12,3 +13,7 @@ export default function Search(props) {
     </InstantSearch>
   );
 }
+
+// This may look invalid, but currently the implementation of search doesn't have dynamic
+// props, and this avoids querying Algolia multiple times + unnecessary re-renders
+export default memo(Search, () => true);
